7c7b23256dad49876bc38dc86a0ee25f2d604680
rayenhdr93 <hedrirayen69@gmail.com>
2023-03-26 04:12:02 +0200

    tryin to catch errors

--- ls/main.c
+++ ls/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
 /**
 * main - main func
 * @argc: num of argum
@@ -11,16 +13,32 @@ int main(int argc, char *argv[])
 {
 	DIR *dir;
 	struct dirent *read;
+	int i = 0;
 
 	if (argc < 2)
 		argv[1] = "./";
-	dir = opendir(argv[1]);
-	while ((read = readdir(dir)) != NULL)
+	for (i = argc - 1; i > 0; i--)
 	{
-		if (read->d_name[0] != '.')
-			printf("%s  ", read->d_name);
+		if (argc > 2)
+			printf("%s:\n", argv[i]);
+		dir = opendir(argv[i]);
+		if (dir) 
+		{
+			while ((read = readdir(dir)) != NULL)
+			if (read->d_name[0] != '.')
+				printf("%s  ", read->d_name);
+			printf("\n");
+			closedir(dir);
+		} 
+		else if (ENOENT == errno)
+		{
+			fprintf(stderr, "./hls: cannot access %s: ", argv[i]);
+			perror("");
+		}
+		else
+			printf("2\n");
+		if (i > 1)
+			printf("\n");
 	}
-	printf("\n");
-	closedir(dir);
 	return (0);
 }
