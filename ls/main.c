5d6301e0c829a3f7ef970325ccefc03b4df61327
rayenhdr93 <hedrirayen69@gmail.com>
2023-03-26 17:35:09 +0100

    hidden files

--- ls/main.c
+++ ls/main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	DIR *dir;
 	struct dirent *read;
 	struct stat buffer;
-	int i = 0, x = 0, nb = argc;
+	int i = 0, x = 0, y=0,nb = argc;
 
 	if (argc < 2)
 		argv[1] = "./";
@@ -24,6 +24,11 @@ int main(int argc, char *argv[])
 		if (argv[i][0] == '-' && argv[i][1] == '1')
 		{
 			x = 1;
+			nb -= 1;
+		}
+		else if (argv[i][0] == '-' && argv[i][1] == 'a')
+		{
+			y = 1;
 			nb -= 1;
 		}
 	}
@@ -40,7 +45,11 @@ int main(int argc, char *argv[])
 			{
 				if (read->d_name[0] != '.' && x == 1)
 					printf("%s\n", read->d_name);
-				else if (read->d_name[0] != '.' )
+				else if (read->d_name[0] != '.' && x == 0)
+					printf("%s  ", read->d_name);
+				else if (y == 1 && x == 1)
+					printf("%s\n", read->d_name);
+				else
 					printf("%s  ", read->d_name);
 			}
 			if (x == 0)
@@ -53,7 +62,7 @@ int main(int argc, char *argv[])
 			{
 				fprintf(stderr, "%s: cannot open directory %s: ", argv[0], argv[i]);
 				perror("");
-				if (i < argc - 1)
+				if (i < nb - 1)
 					printf("\n");
 				continue;
 			}
