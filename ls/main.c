19d75c86500b92ff9364a84015d147b28a82ca2c
rayenhdr93 <hedrirayen69@gmail.com>
2023-03-26 16:35:27 +0100

    -1 option added

--- ls/main.c
+++ ls/main.c
@@ -15,21 +15,36 @@ int main(int argc, char *argv[])
 	DIR *dir;
 	struct dirent *read;
 	struct stat buffer;
-	int i = 0;
+	int i = 0, x = 0, nb = argc;
 
 	if (argc < 2)
 		argv[1] = "./";
 	for (i = 1; i < argc; i++)
 	{
+		if (argv[i][0] == '-' && argv[i][1] == '1')
+		{
+			x = 1;
+			nb -= 1;
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+			continue;
 		dir = opendir(argv[i]);
 		if (dir)
 		{
-			if (argc > 2)
+			if (nb > 2)
 				printf("%s:\n", argv[i]);
 			while ((read = readdir(dir)) != NULL)
-			if (read->d_name[0] != '.')
-				printf("%s  ", read->d_name);
-			printf("\n");
+			{
+				if (read->d_name[0] != '.' && x == 1)
+					printf("%s  \n", read->d_name);
+				else if (read->d_name[0] != '.' )
+					printf("%s  ", read->d_name);
+			}
+			if (x == 0)
+				printf("\n");
 			closedir(dir);
 		}
 		else if (lstat(argv[i], &buffer) == 0)
@@ -44,12 +59,12 @@ int main(int argc, char *argv[])
 			}
 			printf("%s\n", argv[i]);
 		}
-		else 
+		else
 		{
 			fprintf(stderr, "%s: cannot access %s: ", argv[0], argv[i]);
 			perror("");
 		}
-		if (i < argc - 1)
+		if (i < nb - 1)
 			printf("\n");
 	}
 	return (0);
