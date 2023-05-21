e6228ab0b2f028e812c30d8f0246cd1251f232fe
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-21 22:35:48 +0100

    2.c

---
+++ readelf/2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - displays the information contained in the ELF file header
+ * @argc: argc
+ * @argv: argv
+ * @env:  env
+ * Return: return
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
+{
+	char *exe[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	exe[3] = argv[1];
+	if (execve("/usr/bin/readelf", exe, env) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		perror("execv");
+		return (0);
+	}
+}
