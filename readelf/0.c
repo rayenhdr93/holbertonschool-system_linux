ec0f8b03a0bf35b2b8b5e268a1480b5e30e3c0a3
rayenhdr93 <hedrirayen69@gmail.com>
2023-05-21 22:26:29 +0100

    Makefile

---
+++ readelf/0.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program that displays the information contained in the ELF file header of an ELF file
+ * @argc: argc
+ * @argv: argv
+ * @env:  env
+ * Return: return
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
+{
+	char *exe[] = {"/usr/bin/readelf", "-W", "-h", "", NULL};
+
+	exe[3] = argv[1];
+	if (execve("/usr/bin/readelf", exe, env) == 1)
+		return (1);
+	else
+	{
+		perror("execv");
+		return (0);
+	}
+}
