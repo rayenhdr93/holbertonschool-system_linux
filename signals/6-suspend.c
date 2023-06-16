e9fce5a1aff81613d104542208850895f8b8c262
rayenhdr93 <hedrirayen69@gmail.com>
2023-06-16 20:20:09 +0100

    6-suspend.c

---
+++ signals/6-suspend.c
@@ -0,0 +1,24 @@
+#include "signals.h"
+/**
+ * handle_sig - handle sig
+ * @numsig: numsig
+ */
+void handle_sig(int numsig)
+{
+	printf("Caught %d\n", numsig);
+}
+/**
+ * main - program that sets a handler for the signal SIGINT,
+ * and exits right after the signal is received and handled
+ * Return: return
+ */
+int main(void)
+{
+	struct sigaction sa;
+
+	sa.sa_handler = &handler_sig;
+	sigaction(SIGINT, &sa, NULL);
+	pause();
+	printf("Signal received\n");
+	return (EXIT_SUCCESS);
+}
