a30bcf72db48f5fe71535154ddb4f10a662d40ad
rayenhdr93 <hedrirayen69@gmail.com>
2023-06-16 19:41:46 +0100

    3-current_handler_sigaction.c

---
+++ signals/3-current_handler_sigaction.c
@@ -0,0 +1,14 @@
+#include "signals.h"
+/**
+ * current_handler_sigaction - function that retrieves the current
+ * handler of the signal SIGINT
+ * Return:  returns a pointer to the current handler of SIGINT,
+ *  or NULL on failure
+*/
+void (*current_handler_sigaction(void))(int){
+	struct sigaction sig;
+
+	if (sigaction(SIGINT, NULL, &sig) == -1)
+		return (NULL);
+	return (sig.sig_handler);
+}
